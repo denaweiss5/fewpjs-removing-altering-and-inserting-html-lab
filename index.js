603ac50038e5c37b902ce57ebf3bf08a8f5f873f
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-09 16:11:19 -0600

    commit tree 34b0a49f85c9deead8fd33bbbe24957282a7342a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+const removedElement = document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Dena School is the champion!";
